ff12f858b030350354da14646b5f2521b1300494
peter <peter@lourens.cloud>
2025-01-14 18:43:05 +0100

    update read sensors

--- src/read_sensors.cpp
+++ src/read_sensors.cpp
@@ -1,3 +1,31 @@
+/*
+This sensor interacts with the sensors and writes the values to a global variable. The input for 
+this function is the sensors config file which is stored in LittleFS. The measured values must be
+stored in a global variable so other tasks, e.g. statemachine, web inteface, MQTT communication can use this data. 
+Permanent storage of measured data is not required so a two dimensional array would be most suitable
+which is in memery only.
+
+Two arrays of sensors data, one for each I2C multiplexer:
+sensordata_I2C1[8][3]
+sensordata_I2C2[8][3]
+
+The array structure:
+sensordata_I2C1[0][21,50,700] //Sensor0 with three readings: temp, RH, CO2
+sensordata_I2C1[1][22,52,705] //Sensor1 with three readings: temp, RH, CO2
+.....
+sensordata_I2C1[7][21,50,700] //Sensor7 with three readings: temp, RH,CO2
+
+Note that not all sensors have CO2 measurement so the mesasured value stays 0 (as array is initialised with zeros)
+
+Sequence:
+
+1. Read sensor config file
+2. Get measurement data from sensors
+3. Write measured values to varaible
+
+*/
+
+
 #include "read_sensors.h"
 
 SCD4x SCD41;
