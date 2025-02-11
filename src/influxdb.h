13dda871d1f92cf8b4ccad77bcedcb200db04ac2
peter <peter@lourens.cloud>
2025-02-11 15:52:52 +0100

    Added infludb files and defined structure for storing settings

---
+++ src/influxdb.h
@@ -0,0 +1,12 @@
+#ifndef INFLUXDB_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#define INFLUXDB_H
+
+#include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#define DEVICE "ESP32"
+
+// function declarations
+
+#endif
