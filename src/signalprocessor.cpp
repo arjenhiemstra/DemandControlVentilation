cf80667948851b941833d01ac8d0cb4a9ee1cfb4
peter <peter@lourens.cloud>
2025-04-08 21:07:21 +0200

    update signal processor - not working

--- src/signalprocessor.cpp
+++ src/signalprocessor.cpp
@@ -1,5 +1,35 @@
+/*
+
+Rolling  average for sensor
+
+- This function is called after sensor is read. 
+- The oldest value is removed from the array
+- All other values are moved one position up
+- The new value is moved into the first position
+- The new average value of the array is calculated
+- The new average function is stored globally (similar structure to sensor readings)
+
+*/
+
 #include "signalprocessor.h"
 
-void average(void) {
+float average(int bus, int slot, int measurement, float value) {
+
+    float temp[2][8][3][SENSOR_SAMPLES] = {0};
+    float avg;
+    int i;
+
+    if(i==10) {
+        //1st reading is position 0
+        //2nd reading is position 1
+        //..
+        //8th reading
+
+        temp[bus][slot][measurement][i] = value;
+        i=0;
+    }
+    i++;
+
+    return avg;
 
 }
--- src/signalprocessor.h
+++ src/signalprocessor.h
@@ -1,6 +1,8 @@
 #ifndef SIGNALPROCESSOR_H
 #define SIGNALPROCESSOR_H
 
+#define SENSOR_SAMPLES 20
+
 #include <Arduino.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
@@ -8,6 +10,6 @@
 #include "globals.h"
 
 // function declarations
-void average(void);
+float average(int,int,int,float);
 
 #endif
