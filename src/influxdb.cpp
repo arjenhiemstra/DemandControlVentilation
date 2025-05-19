01ee023ff1bc0177fdd18dfce5bbdf84f03cbf71
peter <peter@lourens.cloud>
2025-05-19 18:35:31 +0200

    moved heap stats to influxdb

--- src/influxdb.cpp
+++ src/influxdb.cpp
@@ -275,3 +275,71 @@ void write_fanspeed(void) {
     }
 
 }
+
+void write_heap_info(void) {
+
+    UBaseType_t influxdb_hwm;
+    UBaseType_t i2c_hwm;
+    UBaseType_t mqtt_hwm;
+    UBaseType_t np_hwm;
+    UBaseType_t statemach_hwm;
+    UBaseType_t sys_hwm;
+    UBaseType_t valvectrl_hwm;
+    UBaseType_t web_hwm;
+    UBaseType_t wifi_hwm;
+
+    size_t free_heap_size;
+    size_t minimum_ever_free_heap_size;
+    size_t available_heap_space_bytes;
+    
+    influxdb_hwm = uxTaskGetStackHighWaterMark(task_influxdb);
+    i2c_hwm = uxTaskGetStackHighWaterMark(task_i2c);
+    mqtt_hwm = uxTaskGetStackHighWaterMark(task_mqtt);
+    np_hwm = uxTaskGetStackHighWaterMark(task_np);
+    statemach_hwm = uxTaskGetStackHighWaterMark(task_statemach);
+    sys_hwm = uxTaskGetStackHighWaterMark(task_sys);
+    valvectrl_hwm = uxTaskGetStackHighWaterMark(task_valvectrl);
+    web_hwm = uxTaskGetStackHighWaterMark(h_Task_web);
+    wifi_hwm = uxTaskGetStackHighWaterMark(task_wifi);
+
+    Serial.print("\nTask\t\tHigh water mark");
+    Serial.print("\ntask_influxdb\t\t"); Serial.print(influxdb_hwm);
+    Serial.print("\ntask_i2c\t\t"); Serial.print(i2c_hwm);
+    Serial.print("\ntask_mqtt\t\t"); Serial.print(mqtt_hwm);
+    Serial.print("\ntask_np\t\t"); Serial.print(np_hwm);
+    Serial.print("\ntask_statemach\t\t"); Serial.print(statemach_hwm);
+    Serial.print("\ntask_sys\t\t"); Serial.print(sys_hwm);
+    Serial.print("\ntask_valvectrl\t\t"); Serial.print(valvectrl_hwm);
+    Serial.print("\ntask_web\t\t"); Serial.print(web_hwm);
+    Serial.print("\ntask_wifi\t\t"); Serial.print(wifi_hwm);
+
+    free_heap_size = xPortGetFreeHeapSize();
+    Serial.print("\nFree heap size: ");
+    Serial.print(free_heap_size);
+
+    minimum_ever_free_heap_size = xPortGetMinimumEverFreeHeapSize();
+    Serial.print("\nMinimum ever free heap size: ");
+    Serial.print(minimum_ever_free_heap_size);
+
+    InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+    Point sensor("System_stats");
+    sensor.clearFields();
+    sensor.clearTags();
+    sensor.addField("task_influxdb_hwm", influxdb_hwm);
+    sensor.addField("task_i2c_hwm", i2c_hwm);
+    sensor.addField("task_mqtt_hwm", mqtt_hwm);
+    sensor.addField("task_np_hwm", np_hwm);
+    sensor.addField("task_statemach_hwm", statemach_hwm);
+    sensor.addField("task_sys_hwm", sys_hwm);
+    sensor.addField("task_valvectrl_hwm", valvectrl_hwm);
+    sensor.addField("task_web_hwm", web_hwm);
+    sensor.addField("task_wifi_hwm", wifi_hwm);
+    sensor.addField("min_free_heap_size_ever", minimum_ever_free_heap_size);
+    sensor.addField("free_heap_size", free_heap_size);
+    
+    Serial.print("\nWriting heap info to influxDB.");
+    if (!client.writePoint(sensor)) {
+        Serial.print("\nInfluxDB write failed: ");
+        Serial.print(client.getLastErrorMessage());
+    }
+}
--- src/system.cpp
+++ src/system.cpp
@@ -20,54 +20,9 @@ void task_list(void) {
     Serial.println("\n");
     vTaskDelay(10000);*/
 
-    //Highwatermark
-    UBaseType_t influxdb_hwm;
-    UBaseType_t i2c_hwm;
-    UBaseType_t mqtt_hwm;
-    UBaseType_t np_hwm;
-    UBaseType_t statemach_hwm;
-    UBaseType_t sys_hwm;
-    UBaseType_t valvectrl_hwm;
-    UBaseType_t web_hwm;
-    UBaseType_t wifi_hwm;
-
-    size_t free_heap_size;
-    size_t minimum_ever_free_heap_size;
-    size_t available_heap_space_bytes;
-    
-    influxdb_hwm = uxTaskGetStackHighWaterMark(task_influxdb);
-    i2c_hwm = uxTaskGetStackHighWaterMark(task_i2c);
-    mqtt_hwm = uxTaskGetStackHighWaterMark(task_mqtt);
-    np_hwm = uxTaskGetStackHighWaterMark(task_np);
-    statemach_hwm = uxTaskGetStackHighWaterMark(task_statemach);
-    sys_hwm = uxTaskGetStackHighWaterMark(task_sys);
-    valvectrl_hwm = uxTaskGetStackHighWaterMark(task_valvectrl);
-    web_hwm = uxTaskGetStackHighWaterMark(h_Task_web);
-    wifi_hwm = uxTaskGetStackHighWaterMark(task_wifi);
-
-    Serial.print("\nTask\t\tHigh water mark");
-    Serial.print("\ntask_influxdb\t\t"); Serial.print(influxdb_hwm);
-    Serial.print("\ntask_i2c\t\t"); Serial.print(i2c_hwm);
-    Serial.print("\ntask_mqtt\t\t"); Serial.print(mqtt_hwm);
-    Serial.print("\ntask_np\t\t"); Serial.print(np_hwm);
-    Serial.print("\ntask_statemach\t\t"); Serial.print(statemach_hwm);
-    Serial.print("\ntask_sys\t\t"); Serial.print(sys_hwm);
-    Serial.print("\ntask_valvectrl\t\t"); Serial.print(valvectrl_hwm);
-    Serial.print("\ntask_web\t\t"); Serial.print(web_hwm);
-    Serial.print("\ntask_wifi\t\t"); Serial.print(wifi_hwm);
-    Serial.print("\n");
-
     //available_heap_space_bytes = xAvailableHeapSpaceInBytes();
     //Serial.print("\nAvailble heap space (bytes): ");
     //Serial.print(available_heap_space_bytes);
-
-    free_heap_size = xPortGetFreeHeapSize();
-    Serial.print("\nFree heap size: ");
-    Serial.print(free_heap_size);
-
-    minimum_ever_free_heap_size = xPortGetMinimumEverFreeHeapSize();
-    Serial.print("\nMinimum ever free heap size: ");
-    Serial.print(minimum_ever_free_heap_size);
 
     //HeapStats_t xHeapStats;
     //vPortGetHeapStats(&xHeapStats);
