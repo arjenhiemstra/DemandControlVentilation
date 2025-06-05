6e8052426133e72ccb1d52829e2d910caaeea332
peter <peter@lourens.cloud>
2025-06-05 16:23:25 +0200

    Added bluetooth task

---
+++ src/task_bluetooth.h
@@ -0,0 +1,14 @@
+#ifndef TASK_VALVECONTROL_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#define TASK_VALVECONTROL_H
+
+#include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include "globals.h"
+
+// function declarations
+void start_task_bluetooth(void);
+void task_bluetooth_code(void *pvParameters);
+
+#endif
